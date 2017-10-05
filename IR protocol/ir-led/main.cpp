9e109bbe332ca1adcbb51ff599faf2f43150f560
maartenwassenaar <maartenjanwassenaar@gmail.com>
2017-10-05 12:57:26 +0200

    ir-send-protocol update

--- IR protocol/ir-led/main.cpp
+++ IR protocol/ir-led/main.cpp
@@ -12,33 +12,33 @@ int main( void ){
    auto ir = hwlib::target::d2_36kHz();
    
    // red output LED
-   auto red = hwlib::target::pin_out( hwlib::target::pins::d42 );
+   auto red = hwlib::target::pin_out( hwlib::target::pins::d3 );
    
-   
-   // switch which enables the 36 kHz output
-   //auto sw = hwlib::target::pin_in( hwlib::target::pins::d43 );
-   
-   // when the switch is pressed, 
-   // repeat sending 1 ms signal and 1 ms silence
-   // and show this on the LED
-//   for(;;){
-//      hwlib::wait_ms( 1000 ); 
-//      ir.set( 1 );
-//      red.set( 1 );
-//      
-//      hwlib::wait_ms( 1000 );
-//      ir.set( 0 );
-//      red.set( 0 );
-//   }
     int reeks = 39321;
     bool out = 0;
-    int teller = 0;
-    for(teller = 0;teller<16;teller++){
-        out = (reeks >> teller) & 1;
-        ir.set( out );
-        red.set( out );
-        hwlib::cout<<out<<hwlib::endl;
-        hwlib::wait_ms( 1000 ); 
-   }
+    int teller, Times_Send;
+    for(Times_Send = 0; Times_Send < 2; Times_Send++){
+        for(teller = 0;teller<16;teller++){
+            out = (reeks >> teller) & 1;
+            if( out == 1){
+                ir.set( 1 );
+                red.set( 1 );
+                hwlib::wait_us(800);
+                ir.set( 0 );
+                red.set( 0 );
+                hwlib::wait_us(1600);
+            }
+            else{
+                ir.set( 1 );
+                red.set( 1 );
+                hwlib::wait_us(1600);
+                ir.set( 0 );
+                red.set( 0 );
+                hwlib::wait_us(800);
+            }
+        }
+        hwlib::wait_ms(3);
+    }
 }
+
 
