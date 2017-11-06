3a3a077a2ec9e1405a222b4b71246868e7cf31fb
jeffrey <jeffrey.gek@hotmail.com>
2017-11-06 10:06:38 +0100

    Update

--- Code/Null Versie/IR_Send_controller/IR_Send_Controller/main.cpp
+++ Code/Null Versie/IR_Send_controller/IR_Send_Controller/main.cpp
@@ -4,27 +4,31 @@
 int main(int argc, char **argv)
 {
 // kill the watchdog
-   WDT->WDT_MR = WDT_MR_WDDIS;
-   
-   // wait for the PC terminal to start
-   hwlib::wait_ms( 500 );
-   
-   auto ir_led =  hwlib::target::d2_36kHz();
-   
-   IR_LED ir_send(ir_led);
-   IR_Send_Controller ir_control(ir_send);
-   int player_number = 12;
-   int data = 4;
-   Register_entity register_entity(player_number,data,0);
-   int value = 5524;
-   
-   hwlib::wait_ms( 500 );
-   int signal = ir_control.encode_signal(value, register_entity);
-   hwlib::cout << "\n" <<signal << "\n";
-   for(int i = 0; i < 2; i++)
-    {
+    WDT->WDT_MR = WDT_MR_WDDIS;
+      
+    // wait for the PC terminal to start
+    hwlib::wait_ms( 500 );
+       
+    auto ir_led =  hwlib::target::d2_36kHz();
+       
+    IR_LED ir_send(ir_led);
+    IR_Send_Controller ir_control(ir_send);
+    int player_number = 1;
+    int data = 4;
+    Register_entity register_entity(player_number,data,0);
+    int value = 5524;
+    
+    hwlib::wait_ms( 500 );
+    int signal = ir_control.encode_signal(value, register_entity);
+    hwlib::cout << "\n" <<signal << "\n";
+    for(;;){
+        for(int i = 0; i < 2; i++)
+        {
             ir_control.send_signal(signal);// delay 3244us tussen sender en receiver
             hwlib::wait_us(1);
+        }
+        hwlib::cout << "\n" <<signal << "\n";
+        hwlib::wait_ms(2000);
     }
 //    auto time = hwlib::now_us();
 //    hwlib::cout << "0";
