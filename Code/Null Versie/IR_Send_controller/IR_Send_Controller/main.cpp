319b39ea189fef73ecebc495dded88845d8feb8a
jeffrey <jeffrey.gek@hotmail.com>
2017-11-06 13:06:50 +0100

    update

--- Code/Null Versie/IR_Send_controller/IR_Send_Controller/main.cpp
+++ Code/Null Versie/IR_Send_controller/IR_Send_Controller/main.cpp
@@ -14,13 +14,13 @@ int main(int argc, char **argv)
     IR_LED ir_send(ir_led);
     IR_Send_Controller ir_control(ir_send);
     int player_number = 1;
-    int data = 4;
+    int data = 5;
     Register_entity register_entity(player_number,data,0);
     int value = 5524;
     
     hwlib::wait_ms( 500 );
     int signal = ir_control.encode_signal(value, register_entity);
-    hwlib::cout << "\n" <<signal << "\n";
+    //hwlib::cout << "\n" <<signal << "\n";
     for(;;){
         for(int i = 0; i < 2; i++)
         {
@@ -28,10 +28,10 @@ int main(int argc, char **argv)
             hwlib::wait_us(1);
         }
         hwlib::cout << "\n" <<signal << "\n";
-        hwlib::wait_ms(2000);
+        hwlib::wait_ms(5000);
     }
 //    auto time = hwlib::now_us();
-//    hwlib::cout << "0";
+//    hwlib::cout << "R: pn:data";
 //    int timed = hwlib::now_us()- time;
 //    hwlib::cout << timed;
    
